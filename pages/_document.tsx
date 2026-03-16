ad7c68abc3acec1e3f04bbb64e31124d86ba90ec
dongguacute <gudupao2022@gmail.com>
2026-03-16 18:09:50 +0800

    feat: set document language to zh-CN and add title
    Add Chinese language support and set page title to "SPGA"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" suppressHydrationWarning>
+    <Html lang="zh-CN" suppressHydrationWarning>
       <Head />
+      <title>SPGA</title>
       <body className="antialiased" suppressHydrationWarning>
         <Main />
         <NextScript />
